03941a04436aac357fd659fa281a93f613bc01f6
v-king777 <v.kolpashchikov@gmail.com>
2021-05-20 23:02:05 +0300

    modified:   Task_2/Program.cs

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -11,24 +11,35 @@ namespace Task_2
             try
             {
                 // Читаем текстовый файл с рабочего стола
-                var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                var desktopPath = Environment
+                    .GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
                 var filePath = Path.Combine(desktopPath, "Text1.txt");
                 var text = File.ReadAllText(filePath);
 
                 // Преобразуем текст в массив слов без знаков пунктуации
                 var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
                 var delimiters = new char[] { ' ', '\n', '\r' };
-                var words = noPunctuationText.ToLower().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                var words = noPunctuationText
+                    .ToLower()
+                    .Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
 
-                // Группируем слова по частоте встречаемости
-                var wordGroups = words.GroupBy(w => w)
-                                      .Select(w => new { Word = w.Key, Count = w.Count() })
-                                      .OrderByDescending(w => w.Count);
+                // Группируем слова по частоте встречаемости (операторы LINQ)
+                var wordGroups1 = from a in words
+                                  group a by a into b
+                                  select new { b.Key, Count = b.Count() } into c
+                                  orderby c.Count descending
+                                  select c;
+
+                // Группируем слова по частоте встречаемости (методы расширения LINQ)
+                var wordGroups2 = words
+                    .GroupBy(a => a)
+                    .Select(b => new { b.Key, Count = b.Count() })
+                    .OrderByDescending(c => c.Count);
 
                 // Выводим топ-10 самых встречаемых слов
-                foreach (var w in wordGroups.Take(10))
+                foreach (var word in wordGroups1.Take(10))
                 {
-                    Console.WriteLine($"Слово '{w.Word}' встречается {w.Count} раз(а)");
+                    Console.WriteLine($"Слово '{word.Key}' встречается {word.Count} раз(а)");
                 }
 
                 Console.WriteLine("Нажмите <Enter>...");
